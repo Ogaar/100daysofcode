a837658473faeb3934fa2d9fc9e616bc8ac08fbf
Oscar <oscarborden@hotmail.co.uk>
2023-07-22 14:41:26 +0100

    removed unused files and small refactor

--- woodcutter/woodcutter.py
+++ woodcutter/woodcutter.py
@@ -1,14 +1,10 @@
 # Import packages
 import time
-from datetime import datetime
-
 import mouse as mouse
 import pyautogui
 import cv2
 from PIL import ImageGrab
-
-import bezier_curve_movement
-import random
+import pygetwindow as gw
 import numpy as np
 
 # Required images
@@ -22,15 +18,48 @@ screen_width, screen_height = 1920, 1080  # Modify these values according to you
 # Define function to find and cut down tree
 
 def cut(tree_path):
-    found_tree = False
     # Capture the screen image
     location_x = []
     location_y = []
-    while found_tree == False:
+    num_of_logs = count_logs()
+    if num_of_logs >= 26:
+        drop_logs()
+    if pyautogui.locateOnScreen(tree_path, grayscale=True, confidence=.9):
+        print("Cutting...")
+        pyautogui.click()
         num_of_logs = count_logs()
-        if num_of_logs >= 26:
-            drop_logs()
-        if pyautogui.locateOnScreen(tree_path, grayscale=True, confidence=.9):
+        current_time = time.time()
+        while num_of_logs == count_logs():
+            if time.time() - current_time > 15:
+                break
+            pass
+    else:
+        screen = np.array(ImageGrab.grab(bbox=(0, 0, screen_width, screen_height)))
+        screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+        # Apply shape detection algorithms (example: circle detection using Hough Transform)
+        circles = cv2.HoughCircles(
+            screen,
+            cv2.HOUGH_GRADIENT,
+            dp=1,
+            minDist=150,
+            param1=50,
+            param2=25,
+            minRadius=45,
+            maxRadius=100)
+        circles = np.uint16(np.around(circles))
+        circles = organise_array(circles[0])
+        if circles is not None:
+            # Iterate over detected circles
+
+            for circle in circles:
+                mouse.move(circle[0], circle[1])
+                location_x.append(circle[0])
+                location_y.append(circle[1])
+                if pyautogui.locateOnScreen(tree_path, grayscale=True, confidence=.9):
+                    print("Breaking...")
+                    break
+            mouse.move(location_x[-2],location_y[-2])
+            pyautogui.sleep(.5)
             print("Cutting...")
             pyautogui.click()
             num_of_logs = count_logs()
@@ -39,41 +68,6 @@ def cut(tree_path):
                 if time.time() - current_time > 15:
                     break
                 pass
-        else:
-            screen = np.array(ImageGrab.grab(bbox=(0, 0, screen_width, screen_height)))
-            screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
-            # Apply shape detection algorithms (example: circle detection using Hough Transform)
-            circles = cv2.HoughCircles(
-                screen,
-                cv2.HOUGH_GRADIENT,
-                dp=1,
-                minDist=150,
-                param1=50,
-                param2=25,
-                minRadius=45,
-                maxRadius=100)
-            circles = np.uint16(np.around(circles))
-            circles = organise_array(circles[0])
-            if circles is not None:
-                # Iterate over detected circles
-
-                for circle in circles:
-                    mouse.move(circle[0], circle[1])
-                    location_x.append(circle[0])
-                    location_y.append(circle[1])
-                    if pyautogui.locateOnScreen(tree_path, grayscale=True, confidence=.9):
-                        print("Breaking...")
-                        break
-                mouse.move(location_x[-2],location_y[-2])
-                pyautogui.sleep(.5)
-                print("Cutting...")
-                pyautogui.click()
-                num_of_logs = count_logs()
-                current_time = time.time()
-                while num_of_logs == count_logs():
-                    if time.time() - current_time > 15:
-                        break
-                    pass
 
 
 
@@ -84,9 +78,16 @@ def organise_array(circles):
     return sorted_circles
 
 def count_logs():
+    # Inventory coordinates
+    top_left_inventory_x = 1689
+    top_left_inventory_y = 738
+    bottom_left_inventory_x = 1873
+    bottom_left_inventory_y = 994
+
     image_path = "C:\\Users\\oscar\\OneDrive\\Documents\\100daysofcode\\100daysofcode\\woodcutter\\" \
                             "resources\\inventory_logs.PNG"
-    inventory = np.array(ImageGrab.grab(bbox=(1689, 738, 1873, 994)))
+    inventory = np.array(ImageGrab.grab(bbox=(top_left_inventory_x, top_left_inventory_y, bottom_left_inventory_x, bottom_left_inventory_y
+              )))
     inventory_gray = cv2.cvtColor(inventory, cv2.COLOR_RGB2GRAY)
     image = cv2.imread(image_path, 0)
     result = cv2.matchTemplate(inventory_gray, image, cv2.TM_CCOEFF_NORMED)
@@ -101,9 +102,16 @@ def count_logs():
     return count
 
 def drop_logs():
+    # Inventory coordinates
+    top_left_inventory_x = 1689
+    top_left_inventory_y = 738
+    bottom_left_inventory_x = 1873
+    bottom_left_inventory_y = 994
+
     image_path = "C:\\Users\\oscar\\OneDrive\\Documents\\100daysofcode\\100daysofcode\\woodcutter\\" \
                  "resources\\inventory_logs.PNG"
-    inventory = np.array(ImageGrab.grab(bbox=(1689, 738, 1873, 994)))
+    inventory = np.array(ImageGrab.grab(bbox=(top_left_inventory_x, top_left_inventory_y, bottom_left_inventory_x, bottom_left_inventory_y
+                                              )))
     inventory_gray = cv2.cvtColor(inventory, cv2.COLOR_RGB2GRAY)
     image = cv2.imread(image_path, 0)
     result = cv2.matchTemplate(inventory_gray, image, cv2.TM_CCOEFF_NORMED)
@@ -128,13 +136,17 @@ def drop_logs():
 
 def main():
     tree_choice = input("What type of tree would you like to cut? (Normal/Oak)\n").lower()
+    window_open = True
     if tree_choice == "oak":
         tree_path = chop_down_oak_message_path
     elif tree_choice == "normal":
         tree_path = chop_down_tree_message_path
-    cut(tree_path)
-
-
+    while window_open:
+        active_window = str(gw.getActiveWindow())
+        if "RuneLite" in active_window:
+             cut(tree_path)
+        else:
+            pass
 main()
 
 
